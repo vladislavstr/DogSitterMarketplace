b0f7758a81d6bf0857dc7e7d503507feb2233cba
malinkowork@gmail.com <malinkowork@gmail.com>
2023-03-29 13:58:55 +0300

    update project

--- DogSitterMarketplaceApp/Models/Appeals/Appeal.cs
+++ DogSitterMarketplaceApp/Models/Appeals/Appeal.cs
@@ -3,15 +3,24 @@ using DogSitterMarketplaceApp.Models.Users;
 
 namespace DogSitterMarketplaceApp.Models.Appeals
 {
-    public class Appeal
+    public class AppealOutput
     {
         public int Id { get; set; }
         public string Text { get; set; }
-        public bool IsDeleted { get; set; }
         public TypeOfAppeal Type { get; set; }
         public AppealStatus Status { get; set; }
-        public Order Order { get; set; }
-        public User AppealToUser { get; set; }
+        public Order? Order { get; set; }
         public User AppealFromUser { get; set; }
+        public User? AppealToUser { get; set; }
+    }
+
+    public class AppealIntput
+    {
+        public string Text { get; set; }
+        public int TypeId { get; set; }
+        public int StatusId { get; set; }
+        public int? Order { get; set; }
+        public int AppealFromUser { get; set; }
+        public int? AppealToUser { get; set; }
     }
 }
