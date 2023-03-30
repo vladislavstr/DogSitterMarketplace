ea3453a5ac487dc62e2cc4a148d28378a55d3ac2
VladW <VIStrelnikov94@gmail.com>
2023-03-30 19:14:59 +0300

    Add Bll Request models for User

---
+++ DogSitterMarketplaceBll/Models/Users/Request/UserStatusRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DogSitterMarketplaceBll.Models.Users.Request
+{
+    public class UserStatusRequest
+    {
+        public string Name { get; set; }
+        public string Comment { get; set; }
+    }
+}
