8315cee2b72fe2cec3149ed278ac4840564651a8
VladW <VIStrelnikov94@gmail.com>
2023-03-31 18:42:59 +0300

    Add Response models of User in Api prj

--- DogSitterMarketplaceApi/Models/UsersDto/Response/UserResponseDto.cs
+++ DogSitterMarketplaceApi/Models/UsersDto/Response/UserResponseDto.cs
@@ -1,6 +1,27 @@
-﻿namespace DogSitterMarketplaceApi.Models.UsersDto.Response
+﻿using DogSitterMarketplaceApi.Models.PetsDto.ResponseDto;
+
+namespace DogSitterMarketplaceApi.Models.UsersDto.Response
 {
     public class UserResponseDto
     {
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public UserPassportDataResponseDto? PassportData { get; set; }
+
+        public UserRoleResponseDto Role { get; set; }
+
+        public UserStatusResponseDto Status { get; set; }
+
+        public ICollection<PetResponseDto>? Pets { get; set; }
     }
 }
---
+++ DogSitterMarketplaceApi/Models/UsersDto/Response/UserRolesResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DogSitterMarketplaceApi.Models.UsersDto.Response
+{
+    public class UserRoleResponseDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
