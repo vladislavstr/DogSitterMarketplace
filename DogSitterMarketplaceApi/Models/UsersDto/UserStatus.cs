3584259d7de743176ca142a2d324b378d2c15b72
VladW <VIStrelnikov94@gmail.com>
2023-03-31 18:36:53 +0300

    Add User models in Dal prj

--- DogSitterMarketplaceApi/Models/UsersDto/UserStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DogSitterMarketplaceApi.Models.UsersDto
-{
-    public class UserStatus
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Comment { get; set; }
-    }
-}
