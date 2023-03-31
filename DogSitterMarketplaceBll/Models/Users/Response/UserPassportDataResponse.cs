40179d9591025498a8b5f83f78e8ac9f5ff8ecef
VladW <VIStrelnikov94@gmail.com>
2023-03-31 18:39:43 +0300

    Add Response models of User in Bll prj

---
+++ DogSitterMarketplaceBll/Models/Users/Response/UserPassportDataResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace DogSitterMarketplaceBll.Models.Users.Response
+{
+    public class UserPassportDataResponse
+    {
+        public int Id { get; set; }
+
+        public string PassportNumber { get; set; }
+    }
+}
