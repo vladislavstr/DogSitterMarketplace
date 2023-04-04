995fba06cead06f313273addba008cda1c55117e
VladW <VIStrelnikov94@gmail.com>
2023-04-05 00:01:55 +0300

    Add UserRepository in Dal

---
+++ DogSitterMarketplaceDal/Repositories/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using DogSitterMarketplaceDal.Context;
+using DogSitterMarketplaceDal.Models.Users;
+
+namespace DogSitterMarketplaceDal.Repositories
+{
+    public class UserRepository
+    {
+        private static UserContext context;
+
+        public UserRepository()
+        {
+            context = new UserContext();
+        }
+
+        public UserEntity Create(UserEntity user)
+        {
+            var userDal = new UserEntity
+            {
+
+                Email = user.Email,
+
+                Password = user.Password,
+
+                PhoneNumber = user.PhoneNumber,
+
+                Name = user.Name,
+            };
+
+
+            context.User.Add(userDal);
+            context.SaveChanges();
+
+            return new UserEntity
+            {
+                Id = userDal.Id,
+
+                Email = userDal.Email,
+
+                Password = userDal.Password,
+
+                PhoneNumber = userDal.PhoneNumber,
+
+                Name = userDal.Name,
+            };
+        }
+    }
+}
