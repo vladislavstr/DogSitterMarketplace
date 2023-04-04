29a5fead80f346b4c2bafc6a5bf1ffb8959a049e
VladW <VIStrelnikov94@gmail.com>
2023-04-05 00:01:02 +0300

    Add UserContext

---
+++ DogSitterMarketplaceDal/Context/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using DogSitterMarketplaceDal.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace DogSitterMarketplaceDal.Context
+{
+    public class UserContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseInMemoryDatabase("UserDb");
+        }
+
+        public DbSet<UserEntity> User { get; set; }
+        public DbSet<UserPassportDataEntity> UserPassportData { get; set; }
+        public DbSet<UserRoleEntity> UserRole { get; set; }
+        public DbSet<UserStatusEntity> UserStatus { get; set; }
+    }
+}
