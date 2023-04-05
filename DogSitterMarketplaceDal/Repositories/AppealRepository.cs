c135465f380022073210564a2f79dbbe15edbab3
VladW <VIStrelnikov94@gmail.com>
2023-04-05 19:34:56 +0300

    add AppealRepository

---
+++ DogSitterMarketplaceDal/Repositories/AppealRepository.cs
@@ -0,0 +1,23 @@
+﻿using DogSitterMarketplaceDal.Contexts;
+using DogSitterMarketplaceDal.Models.Appeals;
+
+
+namespace DogSitterMarketplaceDal.Repositories
+{
+    internal class AppealRepository
+    {
+
+        private static AppealContext context;
+
+        public AppealRepository()
+        {
+            context = new AppealContext();
+        }
+
+
+        public IEnumerable<AppealEntity> GetUser()
+        {
+            return context.Appeal.Where(t => !t.IsDeleted).ToList();
+        }
+    }
+}
