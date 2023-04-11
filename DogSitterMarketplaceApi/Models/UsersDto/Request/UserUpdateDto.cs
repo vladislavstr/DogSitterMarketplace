18f521075aab114d5c8bd6bd3ea055fd1fee8728
VladW <VIStrelnikov94@gmail.com>
2023-04-11 18:47:26 +0300

    add UserUpdateDto model in Api

---
+++ DogSitterMarketplaceApi/Models/UsersDto/Request/UserUpdateDto.cs
@@ -0,0 +1,28 @@
+﻿using DogSitterMarketplaceApi.Models.PetsDto.Request;
+
+namespace DogSitterMarketplaceApi.Models.UsersDto.Request
+{
+    public class UserUpdateDto
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public UserPassportDataRequestDto PassportData { get; set; }
+
+        public UserRoleRequestDto Role { get; set; }
+
+        public UserStatusRequestDto? Status { get; set; }
+
+        public ICollection<PetRequestDto>? Pets { get; set; }
+    }
+    {
+    }
+}
