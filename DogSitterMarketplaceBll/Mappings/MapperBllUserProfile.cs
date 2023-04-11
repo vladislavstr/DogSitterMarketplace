c25a70285aa7a568afe56e8f5cf7ccbc0a37310e
VladW <VIStrelnikov94@gmail.com>
2023-04-11 18:49:22 +0300

    Add MapperApiUserProfile in Bll

---
+++ DogSitterMarketplaceBll/Mappings/MapperBllUserProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+using DogSitterMarketplaceDal.Models.Users;
+using DogSitterMarketplaceBll.Models.Users.Response;
+using DogSitterMarketplaceBll.Models.Users.Request;
+
+namespace DogSitterMarketplaceBll.Mappings
+{
+    public class MapperBllUserProfile : Profile
+    {
+        public MapperBllUserProfile()
+        {
+            CreateMap<UserEntity, UserResponse>();
+            CreateMap<UserRequest, UserEntity>();
+            CreateMap<UserUpdate,UserEntity>();
+        }
+    }
+}
