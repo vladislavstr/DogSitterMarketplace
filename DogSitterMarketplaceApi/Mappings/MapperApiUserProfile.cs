ebb9fc1629b93e8256fed8796f00cc7495957661
VladW <VIStrelnikov94@gmail.com>
2023-04-11 18:49:04 +0300

    Add MapperApiUserProfile in Api

---
+++ DogSitterMarketplaceApi/Mappings/MapperApiUserProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DogSitterMarketplaceApi.Models.UsersDto.Response;
+using DogSitterMarketplaceApi.Models.UsersDto.Request;
+using DogSitterMarketplaceBll.Models.Users.Response;
+using DogSitterMarketplaceBll.Models.Users.Request;
+
+
+namespace DogSitterMarketplaceApi.Mappings
+{
+    public class MapperApiUserProfile : Profile
+    {
+        public MapperApiUserProfile()
+        {
+            CreateMap<UserResponse, UserResponseDto>();
+            CreateMap<UserRequestDto, UserRequest>();
+            CreateMap<UserUpdateDto, UserUpdate>();
+        }
+    }
+}
