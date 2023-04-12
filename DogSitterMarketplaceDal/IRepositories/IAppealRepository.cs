b704dea9fb4a7f0678ef996184ec1bfcdf0b0558
VladW <VIStrelnikov94@gmail.com>
2023-04-13 02:48:37 +0300

    add IAppealRepository

---
+++ DogSitterMarketplaceDal/IRepositories/IAppealRepository.cs
@@ -0,0 +1,11 @@
+﻿using DogSitterMarketplaceDal.Models.Appeals;
+
+namespace DogSitterMarketplaceDal.IRepositories
+{
+    public interface IAppealRepository
+    {
+        public ICollection<AppealEntity> GetAllAppeals();
+
+        public AppealEntity AddAppeal(AppealEntity appeal);
+    }
+}
