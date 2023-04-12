7729449d2b14001235b9f643428cfc2090743676
VladW <VIStrelnikov94@gmail.com>
2023-04-13 02:48:54 +0300

    add IUserRepository

---
+++ DogSitterMarketplaceDal/IRepositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DogSitterMarketplaceDal.Models.Users;
+
+namespace DogSitterMarketplaceDal.IRepositories
+{
+    public interface IUserRepository
+    {
+        public ICollection<UserEntity> GetAllUsers();
+
+        public UserEntity AddUser(UserEntity user);
+    }
+}
