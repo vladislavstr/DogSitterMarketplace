3446f5a8f17fe8e6b05d67143f864fbb0b5b153c
malinkowork@gmail.com <malinkowork@gmail.com>
2023-04-23 11:56:36 +0300

    Update Dal project

--- DogSitterMarketplaceDal/IRepositories/IWorkAndLocationRepository.cs
+++ DogSitterMarketplaceDal/IRepositories/IWorkAndLocationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DogSitterMarketplaceDal.Models.Works;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DogSitterMarketplaceDal.IRepositories
 {
-    public interface IWorkRepository
+    public interface IWorkAndLocationRepository
     {
+        public LocationWorkEntity GetLocationWorkByid(int id);
     }
 }
