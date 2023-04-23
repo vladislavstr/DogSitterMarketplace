44f92783d58088ef8f60ab6c431285d0c551b7a7
malinkowork@gmail.com <malinkowork@gmail.com>
2023-04-23 12:04:30 +0300

    add Nlog config

--- DogSitterMarketplaceApi/Program.cs
+++ DogSitterMarketplaceApi/Program.cs
@@ -1,12 +1,31 @@
+
+using DogSitterMarketplaceApi.Mappings;
+using DogSitterMarketplaceBll.IServices;
+using DogSitterMarketplaceBll.Mappings;
+using DogSitterMarketplaceBll.Services;
+using DogSitterMarketplaceDal.IRepositories;
+using DogSitterMarketplaceDal.Repositories;
+using NLog;
+using ILogger = NLog.ILogger;
+using LogManager = NLog.LogManager;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var nlog = LogManager.Setup().GetCurrentClassLogger();
+builder.Services.AddSingleton<ILogger>(nlog);
+builder.Services.AddScoped<IWorkAndLocationRepository, WorkAndLocationRepository>();
+builder.Services.AddScoped<ITimeWorkRepository, TimeWorkRepository>();
+builder.Services.AddScoped<ITimeWorkService, TimeWorkService>();
+builder.Services.AddAutoMapper(typeof(MapperApiWorkProfile), typeof(MapperBllWorkProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
