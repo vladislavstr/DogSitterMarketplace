3f6f42a7665b98d6d00b11abc7ab6a35580e3ab0
malinkowork@gmail.com <malinkowork@gmail.com>
2023-04-23 13:46:02 +0300

    Update code

--- DogSitterMarketplaceBll/Models/Works/Request/WorkTimeRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DogSitterMarketplaceBll.Models.Works.Request
-{
-    public class WorkTimeRequest
-    {
-        public TimeOnly Start { get; set; }
-
-        public TimeOnly Stop { get; set; }
-    }
-}
