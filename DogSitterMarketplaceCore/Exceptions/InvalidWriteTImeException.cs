e65e2edd86be9e8362b968bea92331bc46221288
malinkowork@gmail.com <malinkowork@gmail.com>
2023-04-23 11:56:51 +0300

    Add custom exception

---
+++ DogSitterMarketplaceCore/Exceptions/InvalidWriteTImeException.cs
@@ -0,0 +1,12 @@
+﻿namespace DogSitterMarketplaceCore.Exceptions
+{
+    public class InvalidWriteTimeException : Exception
+    {
+        public string ExceptionValue { get; private set; }
+
+        public InvalidWriteTimeException (string value ) : base ($"The time interval is written incorrectly: {value} ((")
+        {
+            ExceptionValue = value;
+        }
+    }
+}
