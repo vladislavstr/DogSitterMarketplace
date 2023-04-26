9aeb114feea2bf28f65ad280521e6e38dee9d323
malinkowork@gmail.com <malinkowork@gmail.com>
2023-04-27 02:50:13 +0300

    Add Crud Methods Location for DAL

--- DogSitterMarketplaceDal/Repositories/WorkAndLocationRepository.cs
+++ DogSitterMarketplaceDal/Repositories/WorkAndLocationRepository.cs
@@ -2,54 +2,153 @@
 using DogSitterMarketplaceDal.Models.Contexts;
 using DogSitterMarketplaceDal.Models.Works;
 using Microsoft.EntityFrameworkCore;
+using ILogger = NLog.ILogger;
+using LogLevel = NLog.LogLevel;
 
 namespace DogSitterMarketplaceDal.Repositories
 {
     public class WorkAndLocationRepository : IWorkAndLocationRepository
     {
-        private static WorkContext _context;
+        private WorkContext _workContext;
+        private ILogger _logger;
 
-        public WorkAndLocationRepository()
+        public WorkAndLocationRepository(ILogger logger, WorkContext context)
         {
-            _context = new WorkContext();
+            _workContext = context;
+            _logger = logger;
+        }
+
+        public bool AddNewLocationWork(LocationWorkEntity locationWork)
+        {
+            _workContext.LocationWorks.Add(locationWork);
+            _workContext.SaveChanges();
+            _logger.Log(LogLevel.Info, $"Add new location work {locationWork.ToString()}");
+
+            return true;
+        }
+
+        public bool UpdateLocationWork(LocationWorkEntity updateLocationWork)
+        {
+            bool result = false;
+
+            var location = _workContext.LocationWorks
+                .Include(l => l.Location)
+                .Include(l => l.TimingLocationWorks)
+                .Include(l => l.SitterWork)
+                .SingleOrDefault(l => l.Id == updateLocationWork.Id);
+
+            if (location == null)
+            {
+                _logger.Log(LogLevel.Error, $"Location work not fount by id {updateLocationWork.Id} ");
+                throw new FileNotFoundException($"Location work not fount by id {updateLocationWork.Id} ");
+            }
+            else
+            {
+                location.IsNotActive = updateLocationWork.IsNotActive;
+                location.SitterWorkId = updateLocationWork.SitterWorkId;
+                location.LocationId = updateLocationWork.LocationId;
+                location.Price = updateLocationWork.Price;
+                _workContext.SaveChanges();
+                _logger.Log(LogLevel.Info, $"location work is update {location.ToString()}");
+                result = true;
+            }
+
+            return result;
+        }
+
+        public bool DeleteLocationWork(int locationworkId)
+        {
+            bool result = false;
+
+            var deleteLocationWork = _workContext.LocationWorks
+                .SingleOrDefault(l => l.Id == locationworkId);
+
+            if (deleteLocationWork == null)
+            {
+                _logger.Log(LogLevel.Error, $"Location work not fount by id {locationworkId} ");
+                throw new FileNotFoundException($"Location work not fount by id {locationworkId} ");
+            }
+            else
+            {
+                _workContext.LocationWorks.Remove(deleteLocationWork);
+                _workContext.SaveChanges();
+                _logger.Log(LogLevel.Info, $"location work {deleteLocationWork.ToString()} delete");
+                result = true;
+            }
+
+            return result;
+        }
+        public List<LocationWorkEntity> GetAllLocationWork()
+        {
+            var sitterWorks = _workContext.LocationWorks
+                .Include(sw => sw.Location)
+                .Include(sw => sw.TimingLocationWorks)
+                .ThenInclude(sw => sw.DayOfWeek).ToList();
+
+            return sitterWorks;
         }
 
         public LocationWorkEntity GetLocationWorkByid(int id)
         {
-            var location = _context.LocationWorks
+            var location = _workContext.LocationWorks
                 .Include(lw => lw.TimingLocationWorks)
+                .ThenInclude(tl => tl.DayOfWeek)
                 .Include(lw => lw.Location)
                 .Include(lw => lw.SitterWork)
                 .SingleOrDefault(lw => lw.Id == id);
 
+            if (location == null)
+            {
+                _logger.Log(LogLevel.Error, $"Time interval with id {id} not found");
+            }
+
             return location;
         }
 
-        public SitterWorkEntity GetSitterWorkByItsId(int id)
+        public List<LocationWorkEntity> GetAllLocationWorkBySitterWork(int sitterWorkId)
         {
-            var sitterWork = _context.SitterWorks
-                .Include(sw => sw.WorkType)
-                .Include(sw => sw.LocationWork)
-                .ThenInclude(sw => sw.Location)
-                .Include(sw => sw.LocationWork)
-                .ThenInclude(sw => sw.TimingLocationWorks)
-                .ThenInclude(sw => sw.DayOfWeek)
-                .AsNoTracking()
-                .SingleOrDefault(sw => sw.IsDeleted == false && sw.Id == id);
+            if (_workContext.SitterWorks.SingleOrDefault(sw => sw.Id == sitterWorkId)==null)
+            {
+                _logger.Log(LogLevel.Warn, $"Sitter Work  for id {sitterWorkId} not found");
+                throw new FileNotFoundException($"Sitter Work  for id {sitterWorkId} not found");
+            }
 
-            return sitterWork;
+            var sitterWorks = _workContext.LocationWorks
+                .Include(sw => sw.Location)
+                .Include(sw => sw.TimingLocationWorks)
+                .ThenInclude(sw => sw.DayOfWeek)
+                .Where(sw => sw.SitterWorkId == sitterWorkId).ToList();
+
+            if (sitterWorks == null)
+            {
+                _logger.Log(LogLevel.Warn, $"No Time interval for sitter work {sitterWorkId} ");
+            }
+
+            return sitterWorks;
         }
 
-
-        public List<SitterWorkEntity> GetSitterWorksByUserId(int id)
+        public List<LocationWorkEntity> GetAllLocationWorkByLocation(int locationId)
         {
-            var result = _context.SitterWorks
-                .Include(sw => sw.WorkType)
-                .Include(sw => sw.LocationWork)
-                .ThenInclude(sw => sw.Location)
-                .Where(sw => sw.UserId == id).ToList();
+            if (_workContext.LocationWorks.SingleOrDefault(sw => sw.Id == locationId) == null)
+            {
+                _logger.Log(LogLevel.Warn, $"Location by id {locationId} not found");
+                throw new FileNotFoundException($"Location by id {locationId} not found");
+            }
 
-            return result;
+            var sitter = _workContext.LocationWorks
+                .Include(sw => sw.Location)
+                .Include(sw => sw.TimingLocationWorks)
+                .ThenInclude(sw => sw.DayOfWeek)
+                .Include(sw => sw.SitterWork)
+                .Where(sw=>sw.LocationId==locationId).ToList();
+
+            if (sitter == null)
+            {
+                _logger.Log(LogLevel.Warn, $"Location work by location {locationId} not found");
+                throw new FileNotFoundException($"Location work by location {locationId} not found");
+            }
+
+            return sitter;
         }
     }
 }
